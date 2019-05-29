d8b8507b314915e0f3775822cddf41a09bfa488f
epiccodertime! <41688975+epicodertime@users.noreply.github.com>
2019-05-28 21:47:42 -0400

    code uplode
    first python code.

---
+++ piethon code first.py
@@ -0,0 +1,10 @@
+ print("there was big chunguns " + character_name + ",")
+ print("he is 1 year old meme.")
+ print("He was enjoy live as a meme,")
+ print("He died by fortnite players.")
+ character_name ="meme"
+ character_age = 1
+ name= input("Enter your name:")
+ age= input("Enter your age:")
+ print("Hello" + name + "! you are " + age)
+ 
